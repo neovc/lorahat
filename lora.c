feec34d54e2dea2b7e3a1ccaf64039f3721affeb
neovc <gammazeta@icloud.com>
2022-12-26 10:54:06 +0800

    print ascii data of last line.

--- lora.c
+++ lora.c
@@ -100,33 +100,41 @@ print_help(void)
  * Dumps a chunk of memory to the screen
  */
 void
-hex_dump(void *src, int len, int with_header)
+hex_dump(void *p, int len, int with_header)
 {
 	int i, j = 0, k;
-	char text[17];
+	char text[17], *src;
 
-	text[16] = '\0';
+	if (p == NULL || len <= 0)
+		return;
+	src = (char *)p;
 	if (with_header)
 		printf("%p : ", src);
 	for (i = 0; i < len; i ++) {
+		text[j] = src[i];
 		j ++;
-		printf("%02X ", ((volatile unsigned char *)src)[i]);
+		printf("%02X ", src[i]);
 		if (j == 8)
 			printf(" ");
-		if (j == 16) {
-			j = 0;
-			memcpy(text, &((char *)src)[i-15], 16);
-			for (k = 0; k < 16; k ++) {
+		if ((j == 16) || (i == (len - 1))) {
+			/* last line or full of 16 bytes */
+			text[j] = '\0';
+			for (k = 0; k < j; k ++) {
 				if ((text[k] < 32) || (text[k] > 126)) {
 					text[k] = '.';
 				}
 			}
-			printf(" |");
-			printf("%s", text);
-			printf("|\n\r");
+			/* fill necessary space */
+			if (j < 8)
+				printf(" ");
+			for (; j < 16; j ++) {
+				printf("   ");
+			}
+			printf(" %s\r\n", text);
 			if (with_header && (i < (len - 1))) {
 				printf("%p : ", src + i + 1);
 			}
+			j = 0;
 		}
 	}
 
